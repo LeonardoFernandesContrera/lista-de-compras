735658c0273d73dcdcc1a474ac1717d921214376
Leonardo Fernandes Contrera <leonardo.contrera@bkbank.com.br>
2023-09-29 16:28:48 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import styles from "./App.module.css";
+
+import bgImage from "./assets/bg-image.jpg";
+import ShoppingForm from "./components/ShoppingForm";
+
+function App() {
+  return (
+    <div>
+      <header className={styles.header}>
+        <img height={185} src={bgImage} alt="" role="presentation" />
+      </header>
+
+      <main className={styles.main}>
+        <h1>Lista de Compras</h1>
+
+        <ShoppingForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ShoppingForm.jsx
@@ -0,0 +1,48 @@
+import { Plus } from "lucide-react";
+import styles from "./ShoppingForm.module.css";
+
+function ShoppingForm() {
+  return (
+    <form className={styles.shoppingForm}>
+      <div className={styles.formControl}>
+        <label className={styles.label} htmlFor="name">
+          Item
+        </label>
+        <input className={styles.input} type="text" id="name" />
+      </div>
+
+      <div className={styles.formControl}>
+        <label className={styles.label} htmlFor="quantity">
+          Quantidade
+        </label>
+        <div className={styles.qauntityInput}>
+          <input className={styles.input} type="number" id="quantity" />
+          <select id="unit" className={styles.select}>
+            <option value="unidade">Un.</option>
+            <option value="litro">L</option>
+            <option value="kilograma">Kg</option>
+          </select>
+        </div>
+      </div>
+
+      <div className={styles.formControl}>
+        <label className={styles.label} htmlFor="category">
+          Categoria
+        </label>
+        <select className={styles.select} id="category">
+          <option value="padaria">Padaria</option>
+          <option value="legume">legume</option>
+          <option value="carne">Carne</option>
+          <option value="fruta">Fruta</option>
+          <option value="bebida">Bebida</option>
+        </select>
+      </div>
+
+      <button type="submit" aria-label="Adicionar Item">
+        <Plus size={24} />
+      </button>
+    </form>
+  );
+}
+
+export default ShoppingForm;
